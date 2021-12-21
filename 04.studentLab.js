c52f13ba6650d923b76c332039bf008db5cc252a
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-21 13:42:13 +0400

    fix: fixed some bugs

--- 04.studentLab.js
+++ 04.studentLab.js
@@ -1,4 +1,4 @@
-// NEED TO FIX SOME BUGS
+
 /* A teacher has created a gradeLabs function that verifies if student programming labs work. 
 This function loops over an array of JavaScript objects that should contain a student property and runLab property. 
 The runLab property is expected to be a function containing the student's code. 
@@ -9,7 +9,7 @@ If the result and expected results don't match, then the lab is considered a fai
 let studentLabs2 = [
   {
     student: "Blake",
-    myCode: function (num) {
+    runLab: function (num) {
       return Math.pow(num, num);
     },
   },
@@ -28,12 +28,17 @@ let studentLabs2 = [
 ];
 
 function gradeLabs(arrayOfStudents) {
+  let result;
   try {
-    for (let code in arrayOfStudents) {
-      let result = code.runLab(10);
-      console.log(
-        `${code.student} 's code is working, the answer is ${result}`
-      );
+    for (let code of arrayOfStudents) {
+      result = code.runLab(3);
+      if (result === 27) {
+        console.log(
+          `${code.student} 's code is working, the answer is ${result}`
+        );
+      } else {
+        console.log(`the student's answer is wrong`);
+      }
     }
   } catch (err) {
     console.log(`Sorry, there is a ${err.name}, the key "runLab" is missing `);
